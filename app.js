8b6093967cd1df4891f7439ec8e1d8564cdac9b7
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2026-05-11 09:55:16 -0600

    fsdf

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
 const MATCHES = {
   octavos: [
     { id:1,  home:'BR', away:'xJona_YT',                     scoreH:1, scoreA:0, date:'11 Mayo 2026', time:'8:20 AM',   status:'finished',   emoji:['🦁','🐺'] },
+
     { id:2,  home:'Danflo93', away:'Gaitan_gzz',             scoreH:2, scoreA:3, date:'11 Mayo 2026', time:'8:30 AM',   status:'finished',   emoji:['🦅','🐯'] },
     { id:3,  home:'Kyburex', away:'Joshua_smith_503',       scoreH:0, scoreA:1, date:'11 Mayo 2026', time:'8:45 AM',   status:'finished',   emoji:['🐲','🦊'] },
     { id:4,  home:'Ramirez123', away:'lemus_17',             scoreH:0, scoreA:3, date:'11 Mayo 2026', time:'8:45 AM',   status:'finished',   emoji:['🐻','🏹'] },
@@ -17,9 +18,12 @@ const MATCHES = {
 
 
 
-    { id:7,  home:'MauroLH', away:'Noe de Jesús',            scoreH:0, scoreA:0, date:'11 Mayo 2026', time:'9:15 AM',   status:'live',   emoji:['🏹','⚔️'] },
-    { id:8,  home:'XxLABEStiaXx', away:'xJona_YT',                  scoreH:0, scoreA:0, date:'11 Mayo 2026', time:'9:15 AM',   status:'pending',   emoji:['🌟','💥'] },
-    { id:9,  home:'CharlyMart', away:'XxLABEStiaXx',         scoreH:0, scoreA:0, date:'11 Mayo 2026', time:'9:50 AM',   status:'pending',   emoji:['⚡','🔥'] },
+    { id:7,  home:'MauroLH', away:'Noe de Jesús',            scoreH:2, scoreA:3, date:'11 Mayo 2026', time:'9:15 AM',   status:'finished',   emoji:['🏹','⚔️'] },
+    { id:8,  home:'xJona_YT', away:'Emerson_T',                  scoreH:0, scoreA:0, date:'11 Mayo 2026', time:'9:15 AM',   status:'live',   emoji:['🌟','💥'] },
+
+
+
+    { id:9,  home:'Kyburex', away:'xJona_YT',         scoreH:0, scoreA:0, date:'11 Mayo 2026', time:'9:50 AM',   status:'pending',   emoji:['⚡','🔥'] },
   ],
   cuartos: [
     { id:9,   home:'Por definir', away:'Por definir', scoreH:0, scoreA:0, date:'12 Mayo 2026', time:'9:00 AM',  status:'pending', emoji:['🦁','🐲'] },
