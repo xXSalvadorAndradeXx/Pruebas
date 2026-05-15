eacd7f83e74efdf1b26c33553dbed83acd69cc6c
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2026-05-15 08:36:57 -0600

    sad

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const MATCHES = {
   ],
   cuartos: [
     { id:9,   home:'BR', away:'Joshua_Smith', scoreH:0, scoreA:0, date:'15 Mayo 2026', time:'8:00 AM',  status:'pending', emoji:['🦁','🐲'] },
-    { id:10,  home:'Gaitan_gzz', away:'lemus_17', scoreH:5, scoreA:0, date:'15 Mayo 2026', time:'8:00 AM',  status:'live', emoji:['🦁','🐲'] },
+    { id:10,  home:'Gaitan_gzz', away:'lemus_17', scoreH:1, scoreA:5, date:'15 Mayo 2026', time:'8:00 AM',  status:'finished', emoji:['🦁','🐲'] },
 
 
     { id:11,  home:'Noe de Jesús', away:'Lazo26', scoreH:0, scoreA:0, date:'15 Mayo 2026', time:'8:00 AM',  status:'pending', emoji:['🦁','🐲'] },
